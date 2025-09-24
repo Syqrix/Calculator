135de1875700dd3045661f67783a8ae2aae1c8af
Edward Mironov <edwardmir0515@gmail.com>
2025-09-24 17:54:30 +0500

    Should work but have some problem with recurshion

--- calculator.py
+++ calculator.py
@@ -4,18 +4,30 @@ import fractions
 
 class Calculator:
     def __init__(self):
-        self.history = []
+        self.history_of_inputs = []
+        self.user_comunication = TalkingUser()
+        self.program = Operations()
+        self.numbers = UserInput()
+        self.history = History()
 
     def run(self):
-        talker = TalkingUser()
-        history = History()
-        talker.say_hi()
-        self.chose_wish()
-        talker.say_bye()
+        self.user_comunication.say_hi()
+        self.program.start_program()
+        self.user_comunication.say_bye()
 
-    def get_number(self, text="Enter number: "):
+
+class TalkingUser:
+    def say_hi(self):
+        print("Hello, this is calculator app. Type 'q' anytime to quit.")
+
+    def say_bye(self):
+        print("Thanks for using calculator! Bye!")
+
+
+class UserInput:
+    def get_number(self):
         while True:
-            number_str = input(text)
+            number_str = input("Enter number: ")
             if number_str.lower() == "q":
                 return None
             if not number_str:
@@ -25,86 +37,116 @@ class Calculator:
             except ValueError:
                 print("Please enter a valid number!")
 
+
+class History(Calculator):
+    def show_history(self):
+        if not self.history_of_inputs:
+            print("The history is empty!")
+        else:
+            for i in self.history_of_inputs:
+                print(i)
+        self.start_program()
+
+    def clear_history(self):
+        while True:
+            user_answer = input(
+                "Do you really want to clear the history? y/n: ")
+            if user_answer.lower() == "y":
+                break
+            else:
+                if user_answer.lower() == "n":
+                    return None
+                if not user_answer:
+                    print("It's empty, try again!")
+                    continue
+                if user_answer.isdigit():
+                    print("Only numbers!")
+                    continue
+        self.history_of_inputs.clear()
+        self.start_program()
+
+
+class Operations(History):
     def addition(self, a, b):
         try:
-            self.history.append(f"{a} + {b} = {a + b}")
+            self.history_of_inputs.append(f"{a} + {b} = {a + b}")
             print(f"Answer: {a + b}")
         except TypeError:
             if a:
-                self.history.append(
+                self.history_of_inputs.append(
                     f"For make a sum should be 2 number: Answer is {a}")
                 print(f"Answer: {int(a)}")
             if b:
-                self.history.append(
+                self.history_of_inputs.append(
                     f"For make a sum should be 2 number: Answer is {b}")
                 print(f"Answer: {int(b)}")
 
     def subtraction(self, a, b):
         try:
-            self.history.append(f"{a} - {b} = {a - b}")
+            self.history_of_inputs.append(f"{a} - {b} = {a - b}")
             print(f"Answer: {a - b}")
         except TypeError:
             if a:
-                self.history.append(
+                self.history_of_inputs.append(
                     f"For make a subtraction should be 2 number: Answer is {a}")
                 print(f"Answer: {int(a)}")
             if b:
-                self.history.append(
+                self.history_of_inputs.append(
                     f"For make a subtraction should be 2 number: Answer is {b}")
                 print(f"Answer: {int(b)}")
 
     def multiplication(self, a, b):
         try:
-            self.history.append(f"{a} * {b} = {a * b}")
+            self.history_of_inputs.append(f"{a} * {b} = {a * b}")
             print(f"Answer: {a * b}")
         except TypeError:
             if a:
-                self.history.append(
+                self.history_of_inputs.append(
                     f"For make a multiplication should be 2 number: Answer is {a}")
                 print(f"Answer: {int(a)}")
             if b:
-                self.history.append(
+                self.history_of_inputs.append(
                     f"For make a multiplication should be 2 number: Answer is {b}")
                 print(f"Answer: {int(b)}")
 
     def division(self, a, b):
         try:
-            self.history.append(f"{a} / {b} = {a / b}")
+            self.history_of_inputs.append(f"{a} / {b} = {a / b}")
             print(f"Answer: {a / b}")
         except ZeroDivisionError:
             print("You can't divide by 0. Try again.")
         except TypeError:
             if a:
-                self.history.append(
+                self.history_of_inputs.append(
                     f"For make a division should be 2 number: Answer is {a}")
                 print(f"Answer: {int(a)}")
             if b:
-                self.history.append(
+                self.history_of_inputs.append(
                     f"For make a division should be 2 number: Answer is {b}")
                 print(f"Answer: {int(b)}")
 
     def power(self, a, b):
         try:
-            self.history.append(f"{a} ** {b} = {a ** b}")
+            self.history_of_inputs.append(f"{a} ** {b} = {a ** b}")
             print(f"Answer: {a ** b}")
         except TypeError:
             if a:
-                self.history.append(
+                self.history_of_inputs.append(
                     f"For make a power should be 2 number: Answer is {a}")
                 print(f"Answer: {int(a)}")
             if b:
-                self.history.append(
+                self.history_of_inputs.append(
                     f"For make a power should be 2 number: Answer is {b}")
                 print(f"Answer: {int(b)}")
 
     def squart(self, a, b):
         if a:
             result = math.sqrt(a)
-            self.history.append(f"Squart: {a} = {int(result)}")
+            self.history_of_inputs.append(f"Squart: {a} = {int(result)}")
             print(f"Answer {result}")
         if b:
             result = math.sqrt(b)
-            self.history.append(f"Squart: {b} = {int(result)}")
+            self.history_of_inputs.append(f"Squart: {b} = {int(result)}")
             print(f"Answer {result}")
         if a and b:
             print("Only one number! Please try again")
@@ -113,21 +155,21 @@ class Calculator:
         if a:
             a = int(a)
             result = math.factorial(a)
-            self.history.append(f"Factorial: {a} = {result}")
+            self.history_of_inputs.append(f"Factorial: {a} = {result}")
             print(f"Answer {result}")
         if b:
             b = int(b)
             result = math.factorial(b)
-            self.history.append(f"Factorial: {b} = {result}")
+            self.history_of_inputs.append(f"Factorial: {b} = {result}")
             print(f"Answer {result}")
         if a and b:
             print("Only one number! Please try again")
 
-    def chose_wish(self):
+    def start_program(self):
         operations = {
-            1: self.start_program,
-            2: history.show_history(),
-            3: history.clear_history(),
+            1: self.get_input,
+            2: self.show_history,
+            3: self.clear_history,
         }
 
         print("\nAvailable operations:")
@@ -179,52 +221,16 @@ class Calculator:
                 operations[user_answer](a, b)
             return 0
 
-    def start_program(self):
+    def get_input(self):
         while True:
-            first_number = self.get_number("Enter first number: ")
+            first_number = self.numbers.get_number("Enter first number: ")
             if first_number is None:
                 break
-            second_number = self.get_number("Enter second number: ")
+            second_number = self.numbers.get_number("Enter second number: ")
             if second_number is None:
                 break
             if self.chose_math_operation(first_number, second_number) is None:
                 break
-
-
-class History(Calculator):
-    def show_history(self):
-        if not self.history:
-            print("The history is empty!")
-        else:
-            for i in self.history:
-                print(i)
-        self.chose_wish()
-
-    def clear_history(self):
-        while True:
-            user_answer = input(
-                "Do you really want to clear the history? y/n: ")
-            if user_answer.lower() == "y":
-                break
-            else:
-                if user_answer.lower() == "n":
-                    return None
-                if not user_answer:
-                    print("It's empty, try again!")
-                    continue
-                if user_answer.isdigit():
-                    print("Only numbers!")
-                    continue
-        self.history.clear()
-        self.chose_wish()
-
-
-class TalkingUser(Calculator):
-    def say_hi(self):
-        print("Hello, this is calculator app. Type 'q' anytime to quit.")
-
-    def say_bye(self):
-        print("Thanks for using calculator! Bye!")
 
 
 def main():
